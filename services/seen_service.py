47422b641fa07ec67555a24cd2df0b3e32f49904
Nintenzo <m.ahmed.hamdy987@gmail.com>
2025-04-23 16:13:30 +0200

    added selenium_seen

--- services/seen_service.py
+++ services/seen_service.py
@@ -1,3 +1,7 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from cookies_service import get_cookies
 import requests
 import time
 
@@ -8,7 +12,24 @@ def last_seen(remember_user_token, user_session_identifier):
         "user_session_identifier": user_session_identifier,
     }
 
-    response = requests.get(url, cookies=cookies)
+    requests.get(url, cookies=cookies)
     time.sleep(2)
     print("last seen mimicked")
     return
+
+def selenium_seen(driver,wait,email,pw):
+    driver.get("https://login.circle.so/sign_in?request_host=app.circle.so#email")
+    emailform = wait.until(EC.visibility_of_element_located((By.NAME, "user[email]")))
+    emailform.send_keys(email)
+    password = wait.until(EC.visibility_of_element_located((By.NAME, "user[password]")))
+    password.send_keys(pw)
+    password.send_keys(Keys.ENTER)
+    while True:
+        current_url = driver.current_url
+        if current_url == "https://tubiit.circle.so/feed":
+            remember_user_token, user_session_identifier = get_cookies(driver, "remember_user_token","user_session_identifier")
+            driver.quit()
+            print("last seen mimicked")
+            return remember_user_token, user_session_identifier
+        time.sleep(0.5)
+                
